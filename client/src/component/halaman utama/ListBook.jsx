3b3d7effd22c23875ab8eefbb8390f4c265b063b
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-07-09 04:05:10 +0700

    coba

--- client/src/component/halaman utama/ListBook.jsx
+++ client/src/component/halaman utama/ListBook.jsx
@@ -38,6 +38,29 @@ const cards = [
 function ListBook() {
   return (
     <div>
+    <h3 style={{marginLeft : "10%", marginRight : "10%"}}>List Book</h3>
+
+        <div className="cards" style={{marginLeft : "10%", marginRight : "10%", display : "flex", flexWrap : "wrap", marginTop : '20px'}}>      
+          {cards.map((value) => {
+            return <div className="card" style={{color : "black", width : "200px", borderRadius : "5px", margin : "20px"}}>
+              <div className="img">
+                <img src={value.img} alt="" style={{width : "100%"}}/>
+              </div>
+
+              <div className="tittle">
+                <h5>{value.title}</h5>
+              </div>
+
+              <div className="creator">
+                <h6>{value.creator}</h6>
+              </div>
+
+              <div className="price">
+                <p>{value.price}</p>
+              </div>
+            </div>
+          })}  
+        </div>
     </div>
   )
 }
